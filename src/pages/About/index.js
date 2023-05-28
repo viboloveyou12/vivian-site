7f056d8513131214f1ac82d1d094232fd8504224
vivianyang <vivianyang@kkstream.com>
2023-05-28 18:18:37 +0800

    feat: modify about, contact and error pages

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,88 +1,48 @@
-// import './App.scss';
-import { motion } from "framer-motion";
-import { Link } from 'react-router-dom';
-
-const boxVariants = {
-  show: {
-    background: "#fa0", // 顏色會再子元素都跑完後才會觸發
-    transition: {
-        when: "afterChildren", // 再子元素之後才啟動動畫
-        staggerChildren: 0.2,
-    },
-  }
-}
-
-const wordVariants = {
-  show: {
-      opacity: 1,
-      y: 0,
-  },
-  hidden: {
-      opacity: 0,
-      y: 20,
-  },
-};
-
-const containerVariants = {
-  exit: {
-      top: ["100%", "0%", "0%"],
-      bottom: ["0%", "0%", "100%"],
-      transition: {
-          type: "tween",
-          ease: "backInOut",
-          duration: 2,
-      },
-  },
-};
-
-let text = 'Contact page!'
-const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96 ]}
+import { useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from "framer-motion";
+import MainLayout from '../../layout/MainLayout';
+import withTransition from '../../HOC/withTransition';
+import Portrait from '../../img/portrait.png';
+import './style.scss';
 
 function About() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <div className="noise" />
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { ...transition}
-          }}
-          exit={{ opacity: 0, y: 20, transition: {...transition} }}
-        >
-          <Link to="/home">Home</Link>
-          This is about page.
-        </motion.div>
-        <motion.div
-          animate={{
-            x: [0,100,100,200,200,300,300],
-            y: [0,0,100,100,0,0,100]
-          }}
-          transition={{ 
-            // type: "inertia",
-            ease: "circIn",
-            duration: 2
-          }}
-        >123</motion.div>
-        <motion.div
-          initial="hidden"
-          animate="show"
-          variants={boxVariants}
-        >
-          {text.split('').map((word, key) => (
-            <motion.span
-              key={word+key}
-              variants={wordVariants}
-            >
-              {word}
-            </motion.span>
-          ))}
-        </motion.div>
-      </header>
-    </div>
+    <MainLayout path="/about">
+      <main className="about">
+        <div className="about-left">
+          <h1>About</h1>
+          <img src={Portrait} alt="portrait"></img>
+        </div>
+        <div className="about-right">
+          <h2>
+          I'm Vivian Yang, a front end developer and problem solver.
+          </h2>
+          <p>
+          Born in 1996 in Taipei, Taiwan.
+          <br></br>
+          I have three years of experience in web development, I have worked in various industries from cloud service to OTT streaming service to build web applications. 
+          Back in 2018, when I was specialized in Interactive Design, I was fascinated with the digital world and coding, and what excites me most about working in software development is being able to solve real problems and have a positive impact in the word. 
+          </p>
+          <h3>SKILLS</h3>
+          <div className="about-skills">
+            <div>AWS</div>
+            <div>Node.js</div>
+            <div>React.js</div>
+            <div>Next.js</div>
+            <div>Google Analytic</div>
+            <div>Google Tag Manager</div>
+            <div>Next.js</div>
+            <div>CMS</div>
+            <div>Git</div>
+            <div>AWS</div>
+            <div>Node.js</div>
+            <div>React.js</div>
+          </div>
+        </div>
+      </main>
+    </MainLayout>
   );
 }
 
-export default About;
+export default withTransition(About);
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,82 +1,63 @@
-// import './App.scss';
-import { motion } from "framer-motion";
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
-const boxVariants = {
-  show: {
-    background: "#fa0", // 顏色會再子元素都跑完後才會觸發
-    transition: {
-        when: "afterChildren", // 再子元素之後才啟動動畫
-        staggerChildren: 0.2,
-    },
-  }
-}
-
-const wordVariants = {
-  show: {
-      opacity: 1,
-      y: 0,
-  },
-  hidden: {
-      opacity: 0,
-      y: 20,
-  },
-};
-
-const containerVariants = {
-  exit: {
-      top: ["100%", "0%", "0%"],
-      bottom: ["0%", "0%", "100%"],
-      transition: {
-          type: "tween",
-          ease: "backInOut",
-          duration: 2,
-      },
-  },
-};
-
-const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96 ]}
-
-let text = 'Contact page!'
+import MainLayout from '../../layout/MainLayout';
+import withTransition from '../../HOC/withTransition';
+import Star from '../../img/star.svg';
+import './style.scss';
 
 function Contact() {
+  const [showLoading, setShowLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoading(false)
+    }, 4000)
+    document.body.classList.add('black-bg', 'no-verticalScroll');
+    return () => {
+      document.body.classList.remove('black-bg', 'no-verticalScroll');
+    }
+  }, [])
+
   return (
-    <motion.div 
-      className="App"
-      exit={{ opacity: 0}}
-      transition={transition}
-    >
-      <header className="App-header">
-          <Link to="/home">Go to Home</Link>
-          This is contact page.
-        <motion.div
-          animate={{
-            x: [0,100,100,200,200,300,300],
-            y: [0,0,100,100,0,0,100]
-          }}
-          transition={{ 
-            // type: "inertia",
-            ease: "circIn",
-            duration: 2
-          }}
-        >123</motion.div>
-        <motion.div
-          initial="hidden"
-          animate="show"
-          variants={boxVariants}
-        >
-          {text.split('').map((word, key) => (
-            <motion.span
-              key={word+key}
-              variants={wordVariants}
-            >
-              {word}
-            </motion.span>
-          ))}
-        </motion.div>
-      </header>
-    </motion.div>
-  );
+    <MainLayout path="/contact">
+      {/* {showLoading && <Loading/>} */}
+      <main className="contact">
+        <div className="contact-content">
+          {/* <Marquee
+            style={{width: '370px'}}
+            speed={60}
+          >
+            <h1>Contact. </h1>
+          </Marquee> */}
+          <div className="top"
+          >
+            <h1>
+              Reach <img src={Star}></img>
+              <br></br>
+              Out Today
+            </h1>
+            <p>
+              HI, I’m currently looking for new opportunities.<br></br> 
+              Whether you have a question or just want to say hi,<br></br> 
+              I’ll try my best to get back to you :)
+            </p>
+          </div>
+          <div className="decorate-line"></div>
+          <div className="bottom">
+            <div className="bottom-left">
+              <Link to="/home">Home</Link>
+              <Link to="/home">Work</Link>
+              <Link to="/home">About</Link>
+            </div>
+            <div className="bottom-right">
+              <Link to="/home">Linkedln</Link>
+              <Link to="/home">Github</Link>
+              <Link to="/home">Email</Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </MainLayout>
+  )
 }
 
-export default Contact;
+export default withTransition(Contact);
