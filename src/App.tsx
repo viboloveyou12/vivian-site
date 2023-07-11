a6e089030842e819bfdf35ad5ce6a9e432da5d00
VivianYang <viboloveyou12@gmail.com>
2023-07-11 12:47:44 +0800

    feat: add the ga tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactGA from "react-ga4";
 import './App.scss';
 import { AnimatePresence } from "framer-motion";
 import { useLocation, useRoutes } from 'react-router-dom';
@@ -7,6 +8,7 @@ import CursorContextProvider from './context/cursorContext';
 import Cursor from './components/Cursor';
 import './styles/common.scss';
 
+ReactGA.initialize("G-5S4VHGE868");
 
 function App() {
   const location = useLocation();
