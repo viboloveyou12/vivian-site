f2bf1ef4aeb2755898981922dd2f6a059647651b
VivianYang <viboloveyou12@gmail.com>
2023-07-11 12:47:30 +0800

    feat: preload the font

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import './App.scss';
 import { AnimatePresence } from "framer-motion";
 import { useLocation, useRoutes } from 'react-router-dom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import routesConfig from './routes/config';
 import CursorContextProvider from './context/cursorContext';
 import Cursor from './components/Cursor';
+import './styles/common.scss';
 
 
 function App() {
@@ -15,18 +15,12 @@ function App() {
   if (!element) return null;
 
   return (
-    <HelmetProvider>
-      <Helmet>
-          <link rel="preload" href="https://fonts.cdnfonts.com/css/gallient" as="font" crossOrigin="anonymous" />
-          <link rel="preload" href="https://fonts.cdnfonts.com/s/53659/GallientRegular-eZoMp.woff" as="font" crossOrigin="anonymous" />
-      </Helmet>
-      <CursorContextProvider>
-        <AnimatePresence mode="wait">
-          {React.cloneElement(element, { location: location, key: location.pathname })}
-        </AnimatePresence>
-        <Cursor/>
+    <CursorContextProvider>
+      <AnimatePresence mode="wait">
+        {React.cloneElement(element, { location: location, key: location.pathname })}
+      </AnimatePresence>
+      <Cursor/>
     </CursorContextProvider>
-    </HelmetProvider>
   );
 }
 
